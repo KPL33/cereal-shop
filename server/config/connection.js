8321d31fdc5f0da5fdfccae5311230a240db8cf9
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-24 16:32:30 -0700

    Changes made to connection file, to align with Heroku expectations

--- server/config/connection.js
+++ server/config/connection.js
@@ -11,15 +11,33 @@ const __dirname = dirname(__filename);
 // Load environment variables from .env file
 config({ path: path.resolve(__dirname, "../../.env") });
 
-// Extract environment variables
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } =
-  process.env;
+// Extract environment variables or use defaults
+const {
+  DATABASE_URL,
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_DIALECT = "mysql",
+} = process.env;
 
 // Create Sequelize instance
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_DIALECT || "mysql", // Use "mysql" as the default dialect
-});
+const sequelize = DATABASE_URL
+  ? new Sequelize(DATABASE_URL, {
+      dialect: DB_DIALECT,
+      protocol: "mysql",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+      host: DB_HOST,
+      port: DB_PORT,
+      dialect: DB_DIALECT || "mysql",
+    });
 
 export default sequelize;
