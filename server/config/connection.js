59a3e91b337ab544ee329fe69d96086e07210b61
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-25 13:52:40 -0700

    deploying...

--- server/config/connection.js
+++ server/config/connection.js
@@ -19,7 +19,8 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } =
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: DB_DIALECT || "mysql", // Use "mysql" as the default dialect
+  dialect: DB_DIALECT || "mysql",
+  logging: false,
 });
 
 export default sequelize;
