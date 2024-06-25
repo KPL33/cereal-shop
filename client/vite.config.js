104a7f3de2847b6903cc6807c67d9312f73e0cb8
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-25 14:12:55 -0700

    vite config adjust to help with Heroku deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import nodemailer from 'nodemailer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        nodemailer,
-      ],
+      external: ["nodemailer"], // Keep nodemailer as an external dependency
     },
   },
   plugins: [react()],
+  css: {
+    modules: false, // Enable or disable CSS modules as needed
+    // Additional CSS configuration can be added here
+  },
 });
