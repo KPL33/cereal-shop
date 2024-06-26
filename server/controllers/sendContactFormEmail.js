6ad2f1a401ad90d1d4d891b170e5ed737a8b35b9
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-26 12:02:08 -0700

    Resetting for yet another Heroku deployment attempt...

--- server/controllers/sendContactFormEmail.js
+++ server/controllers/sendContactFormEmail.js
@@ -1,29 +1,11 @@
-import nodemailer from "nodemailer";
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.mail.yahoo.com", // SMTP server address for Yahoo Mail
-  port: 465, // SMTP port (SSL) for Yahoo Mail
-  secure: true, // Use SSL/TLS
-  auth: {
-    user: process.env.EMAIL_USER, // Your Yahoo email address
-    pass: process.env.EMAIL_PASS, // Your Yahoo email password
-  },
-  debug: true, // Enable debug mode for detailed SMTP communication
-});
+import { sendContact } from "../../utils/emailer.mjs";
 
 const sendContactFormEmailController = async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
     // Send the contact email
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER, // Sender email address
-      to: "gitajob@yahoo.com", // Recipient email address (replace with your recipient)
-      subject: "New message from contact form",
-      html: `<p><strong>Name:</strong> ${name}</p>
-            <p><strong>Email:</strong> ${email}</p>
-            <p><strong>Message:</strong> ${message}</p>`,
-    });
+    await sendContact(name, email, message);
 
     // Send response
     res.status(200).send("Email sent successfully");
