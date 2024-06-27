85002fdd7a82de050f3b23daaea8c3fb0643b829
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-27 14:02:13 -0700

    attempting deployment on Netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import nodemailer from 'nodemailer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["nodemailer"],
+      external: [
+        nodemailer,
+      ],
     },
   },
   plugins: [react()],
