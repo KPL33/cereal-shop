22b0b2794827d4375eaa89a5fb9bf5f7dd59a28d
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-06-27 13:50:29 -0700

    nodemailer now external dependency in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import nodemailer from 'nodemailer';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [
-        nodemailer,
-      ],
+      external: ["nodemailer"],
     },
   },
   plugins: [react()],
