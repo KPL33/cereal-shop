2c7678f077ad9fd532e2539193c61761b779155f
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-02 16:34:49 -0700

    adjusted config file

--- server/config/connection.js
+++ server/config/connection.js
@@ -12,14 +12,49 @@ const __dirname = dirname(__filename);
 config({ path: path.resolve(__dirname, "../../.env") });
 
 // Extract environment variables
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DIALECT } =
-  process.env;
+const {
+  DB_NAME,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_DIALECT,
+  NODE_ENV,
+} = process.env;
+
+// Define default configurations
+const defaultConfig = {
+  development: {
+    DB_NAME: DB_NAME || "development_db",
+    DB_USER: DB_USER || "root",
+    DB_PASSWORD: DB_PASSWORD || "",
+    DB_HOST: DB_HOST || "localhost",
+    DB_PORT: DB_PORT || 3306,
+    DB_DIALECT: DB_DIALECT || "mysql",
+  },
+  production: {
+    DB_NAME: DB_NAME || "production_db",
+    DB_USER: DB_USER || "prod_user",
+    DB_PASSWORD: DB_PASSWORD || "",
+    DB_HOST: DB_HOST || "prod.example.com",
+    DB_PORT: DB_PORT || 3306,
+    DB_DIALECT: DB_DIALECT || "mysql",
+  },
+};
+
+// Select configuration based on NODE_ENV or default to development
+const configOptions = defaultConfig[NODE_ENV || "development"];
 
 // Create Sequelize instance
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_DIALECT || "mysql", // Use "mysql" as the default dialect
-});
+const sequelize = new Sequelize(
+  configOptions.DB_NAME,
+  configOptions.DB_USER,
+  configOptions.DB_PASSWORD,
+  {
+    host: configOptions.DB_HOST,
+    port: configOptions.DB_PORT,
+    dialect: configOptions.DB_DIALECT,
+  }
+);
 
 export default sequelize;
