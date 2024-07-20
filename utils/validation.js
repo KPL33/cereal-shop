ce4b3e19ed73d8d4b7dcc6486e4cf0eb1fee5e7f
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-20 12:54:14 -0700

    Hello World attempt on heroku

--- utils/validation.js
+++
@@ -1,9 +0,0 @@
-const passwordRegex =
-/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+}{"':;?/>.<,]).{8,50}$/;
-
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-module.exports = {
-  emailRegex,
-  passwordRegex,
-};
