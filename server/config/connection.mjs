c257e1a8678451e32a821d8cfa74734b289947bb
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-22 11:18:37 -0700

    adjust connection.mjs

--- server/config/connection.mjs
+++ server/config/connection.mjs
@@ -4,14 +4,11 @@ import { Sequelize } from "sequelize";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
-// Get directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-// Load environment variables from .env file
 config({ path: path.resolve(__dirname, "../../.env") });
 
-// Extract environment variables
 const {
   DB_NAME,
   DB_USER,
@@ -22,13 +19,28 @@ const {
   JAWSDB_URL,
 } = process.env;
 
-// Create Sequelize instance
-const sequelize = JAWSDB_URL
-  ? new Sequelize(JAWSDB_URL, { dialect: "mysql" })
-  : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+// const sequelize = JAWSDB_URL
+//   ? new Sequelize(JAWSDB_URL, { dialect: "mysql" })
+//   : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//       host: DB_HOST,
+//       port: DB_PORT,
+//       dialect: DB_DIALECT || "mysql",
+//     });
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
       host: DB_HOST,
       port: DB_PORT,
-      dialect: DB_DIALECT || "mysql",
-    });
-
+      dialect: DB_DIALECT
+    }
+  );
+}
 export default sequelize;
