219d9c430f2eb9151480acb8fa55e1234d87e180
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-23 11:56:21 -0700

    static path reset, part 3

--- server.mjs
+++ server.mjs
@@ -1,15 +1,15 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'client/dist' directory
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // Handle all other routes and serve index.html from 'client/dist'
-app.get("*", (res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(PORT, () => {
