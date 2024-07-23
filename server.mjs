41388c09801df0fcb8c7c248b312d0b8f410c140
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-22 21:33:29 -0700

    Adjusted port in server.mjs

--- server.mjs
+++ server.mjs
@@ -17,6 +17,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
