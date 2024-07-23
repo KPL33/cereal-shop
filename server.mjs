f58ec416ed0aa8439b88555b5b49aba1335677b2
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-23 14:01:05 -0700

    static path reset, part 9

--- server.mjs
+++ server.mjs
@@ -3,17 +3,20 @@ import path from "path";
 
 const app = express();
 
+// Calculate paths relative to the module location
+const clientDistPath = path.join("client", "dist");
+const indexPath = path.join(clientDistPath, "index.html");
+
 // Serve static files from the dist directory
-app.use(express.static(path.resolve("./client/dist")));
+app.use(express.static(clientDistPath));
 
 // Define a route for the root path
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("./client/dist/index.html"));
+  res.sendFile(indexPath, { root: "." });
 });
-console.log(`Serving static files from ${path.resolve("./client/dist")}`);
-console.log(
-  `Serving index.html from ${path.resolve("./client/dist/index.html")}`
-);
+
+console.log(`Serving static files from ${clientDistPath}`);
+console.log(`Serving index.html from ${indexPath}`);
 
 // Handle other routes or API endpoints here
 
