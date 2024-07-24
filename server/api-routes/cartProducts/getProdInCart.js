07ae5d0562a71524e7629882c659005c1e9c7a5d
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-24 12:46:56 -0700

    cartProducts RESTful methods work locally

--- server/api-routes/cartProducts/getProdInCart.js
+++ server/api-routes/cartProducts/getProdInCart.js
@@ -1,15 +1,27 @@
 import express from "express";
-import getAllProductsInCart from "../../controllers/prodInCart/getProdInCart.js";
+import getProductsInCart from "../../controllers/prodInCart/getProdInCart.js";
 
 const router = express.Router();
 
 // Endpoint to retrieve all products in a user's cart
 router.get("/", async (req, res) => {
   try {
-    const productsInCart = await getAllProductsInCart();
-    return res.status(200).json(productsInCart); // Return the products in the cart
+    const productsInCart = await getProductsInCart();
+    return res.status(200).json(productsInCart); // Return all products in the cart
   } catch (error) {
     console.error("Error getting products in cart:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+// Endpoint to retrieve a specific product in the cart by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const productInCart = await getProductsInCart(id);
+    return res.status(200).json(productInCart); // Return the product in the cart
+  } catch (error) {
+    console.error("Error getting product in cart:", error);
     return res.status(500).json({ message: "Internal server error" });
   }
 });
--- server/controllers/prodInCart/getProdInCart.js
+++ server/controllers/prodInCart/getProdInCart.js
@@ -2,26 +2,49 @@ import CartProduct from "../../models/CartProduct.js";
 import User from "../../models/User.js";
 import Product from "../../models/Product.js";
 
-//Because we wouldn't need to get just 1 CartProduct, we don't include a getById.
-const getAllProductsInCart = async () => {
+// Function to get all products in the cart or a specific product by ID
+const getProductsInCart = async (id = null) => {
   try {
-    const productsInCart = await CartProduct.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["id", "email", "firstName", "lastName"], // Include specific attributes of User
-        },
-        {
-          model: Product,
-          attributes: ["id", "name"], // Include specific attributes of Product
-        },
-      ],
-    });
+    if (id) {
+      // If an ID is provided, find the specific CartProduct by ID
+      const productInCart = await CartProduct.findByPk(id, {
+        include: [
+          {
+            model: User,
+            attributes: ["id", "email", "firstName", "lastName"], // Include specific attributes of User
+          },
+          {
+            model: Product,
+            attributes: ["id", "name"], // Include specific attributes of Product
+          },
+        ],
+      });
 
-    return productsInCart;
+      if (!productInCart) {
+        throw new Error("Product not found in cart");
+      }
+
+      return productInCart;
+    } else {
+      // If no ID is provided, get all CartProducts
+      const productsInCart = await CartProduct.findAll({
+        include: [
+          {
+            model: User,
+            attributes: ["id", "email", "firstName", "lastName"], // Include specific attributes of User
+          },
+          {
+            model: Product,
+            attributes: ["id", "name"], // Include specific attributes of Product
+          },
+        ],
+      });
+
+      return productsInCart;
+    }
   } catch (error) {
     throw error;
   }
 };
 
-export default getAllProductsInCart;
+export default getProductsInCart;
