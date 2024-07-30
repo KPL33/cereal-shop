249db109e327db8264cdfd9affb8184cd8a8592e
Kevin Lewis <20131699+KPL33@users.noreply.github.com>
2024-07-30 15:54:47 -0700

    Added cart empty message, minor css adjustments made to nav, head, cart and prod-select components

--- client/src/components/Main/Cart/Cart.jsx
+++ client/src/components/Main/Cart/Cart.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useContext } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { AppContext } from "../../../context/AppContext";
 import axios from "axios";
 import "./cart.css";
@@ -20,7 +20,7 @@ const Cart = () => {
   const navigate = useNavigate();
 
   const handleShopping = () => {
-    navigate("/products"); // Navigate to the checkout page
+    navigate("/products"); // Navigate to the products page
   };
 
   const handleCheckout = () => {
@@ -28,7 +28,6 @@ const Cart = () => {
   };
 
   const handleInputBlur = (productId, currentQuantity) => {
-    // If input is empty, set quantity to 0
     if (currentQuantity === "") {
       handleQuantityChange(productId, "0");
       setShowDefaultZero(true);
@@ -38,19 +37,17 @@ const Cart = () => {
   };
 
   const handleQuantityChange = async (productId, newQuantity) => {
-    // Convert empty string to 0
     if (newQuantity === "") {
       newQuantity = 0;
     } else {
       newQuantity = parseInt(newQuantity);
     }
 
-    // Check if quantity exceeds 99
     if (newQuantity > 99) {
       setError("Please adjust quantity. Cannot exceed 99 per item.");
       return;
     } else {
-      setError(""); // Clear error if quantity is valid
+      setError("");
     }
 
     const updatedProducts = cartProducts.map((product) => {
@@ -58,7 +55,7 @@ const Cart = () => {
         const updatedProduct = {
           ...product,
           productQuantity: newQuantity,
-          productTotal: (product.productPrice * newQuantity).toFixed(2), // Update product total
+          productTotal: (product.productPrice * newQuantity).toFixed(2),
         };
         return updatedProduct;
       }
@@ -67,7 +64,6 @@ const Cart = () => {
 
     setCartProducts(updatedProducts);
 
-    // Send PUT request to update quantity in the backend
     const currentCartId = localStorage.getItem("currentCartId");
     if (currentCartId) {
       try {
@@ -75,10 +71,8 @@ const Cart = () => {
           cartId: currentCartId,
           quantity: newQuantity,
         });
-        // Handle success if needed
       } catch (error) {
         console.error("Error updating quantity:", error);
-        // Handle error if needed
       }
     }
   };
@@ -108,12 +102,10 @@ const Cart = () => {
         const response = await axios.get(`${apiUrl}/cart/products`);
         const cartData = response.data;
 
-        // Filter CartProducts belonging to the current cart
         const cartProducts = cartData.filter(
           (product) => product.cartId === parseInt(currentCartId)
         );
 
-        // Populate the cart component on the frontend with fetched CartProducts
         setCartProducts(cartProducts);
       } catch (error) {
         console.error("Error fetching cart products:", error);
@@ -127,12 +119,10 @@ const Cart = () => {
     try {
       const currentCartId = localStorage.getItem("currentCartId");
       if (currentCartId) {
-        // Send DELETE request to backend API
         await axios.delete(`${apiUrl}/cart/products/${productId}`, {
           data: { cartId: currentCartId },
         });
 
-        // Remove the deleted product from the cartProducts state
         const updatedCartProducts = cartProducts.filter(
           (product) => product.id !== productId
         );
@@ -145,107 +135,126 @@ const Cart = () => {
 
   return (
     <section className="cart">
-      <div className="cart-container">
-        <h2 className="title">Here&apos;s your cart!</h2>
-        <div className="in-cart">
-          <div className="category-title-area">
-            <div className="product-column">
-              <h3 className="column-title product-column-title">Product</h3>
-            </div>
-            <div className="quantity-column">
-              <h3 className="column-title">Quantity</h3>
-            </div>
-            <div className="price-column">
-              <h3 className="column-title">Price</h3>
-            </div>
-            <div className="prod-total-column">
-              <h3 className="column-title">Product Total</h3>
-            </div>
-          </div>
-
-          {cartProducts.map((product) => (
-            <div key={product.id} className="cart-items">
-              <div className="product-column">
-                <div className="product-name-container">
-                  <button
-                    className="delete-from-cart-button"
-                    onClick={() => handleDeleteProduct(product.id)}
-                  >
-                    Remove
-                  </button>
-                  <p className="product-name">{product.Product.name}</p>
+      <div
+        className={`cart-container ${
+          cartProducts.length === 0 ? "cart-empty" : ""
+        }`}
+      >
+        {cartProducts.length === 0 ? (
+          <h2 className="title">
+            Your cart is empty! Please select a{" "}
+            <Link className="to-prods" to="/products">
+              Product
+            </Link>{" "}
+            before Checkout.
+          </h2>
+        ) : (
+          <>
+            <h2 className="title">Here&apos;s your cart!</h2>
+            <div className="in-cart">
+              <div className="category-title-area">
+                <div className="product-column">
+                  <h3 className="column-title product-column-title">Product</h3>
+                </div>
+                <div className="quantity-column">
+                  <h3 className="column-title">Quantity</h3>
+                </div>
+                <div className="price-column">
+                  <h3 className="column-title">Price</h3>
+                </div>
+                <div className="prod-total-column">
+                  <h3 className="column-title">Product Total</h3>
                 </div>
               </div>
 
-              <div className="quantity-column">
-                <div className="product-quantity">
-                  <button
-                    onClick={() => handleDecrement(product.id)}
-                    className="cart-quantity-button"
-                    disabled={product.productQuantity <= 1}
-                  >
-                    –
-                  </button>
-                  <input
-                    type="text"
-                    value={showDefaultZero ? "0" : product.productQuantity}
-                    onChange={(e) =>
-                      handleQuantityChange(product.id, e.target.value)
-                    }
-                    onBlur={(e) => handleInputBlur(product.id, e.target.value)}
-                    className="product-quantity-input"
-                  />
-                  <button
-                    onClick={() => handleIncrement(product.id)}
-                    className="cart-quantity-button"
-                    disabled={product.productQuantity >= 99}
-                  >
-                    +
-                  </button>
+              {cartProducts.map((product) => (
+                <div key={product.id} className="cart-items">
+                  <div className="product-column">
+                    <div className="product-name-container">
+                      <button
+                        className="delete-from-cart-button"
+                        onClick={() => handleDeleteProduct(product.id)}
+                      >
+                        Remove
+                      </button>
+                      <p className="product-name">{product.Product.name}</p>
+                    </div>
+                  </div>
+
+                  <div className="quantity-column">
+                    <div className="product-quantity">
+                      <button
+                        onClick={() => handleDecrement(product.id)}
+                        className="cart-quantity-button"
+                        disabled={product.productQuantity <= 1}
+                      >
+                        –
+                      </button>
+                      <input
+                        type="text"
+                        value={showDefaultZero ? "0" : product.productQuantity}
+                        onChange={(e) =>
+                          handleQuantityChange(product.id, e.target.value)
+                        }
+                        onBlur={(e) =>
+                          handleInputBlur(product.id, e.target.value)
+                        }
+                        className="product-quantity-input"
+                      />
+                      <button
+                        onClick={() => handleIncrement(product.id)}
+                        className="cart-quantity-button"
+                        disabled={product.productQuantity >= 99}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+
+                  <div className="price-column">
+                    <p className="product-price">${product.productPrice}</p>
+                  </div>
+
+                  <div className="prod-total-column">
+                    <p className="product-total-price">
+                      $
+                      {product.productTotal
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                    </p>
+                  </div>
                 </div>
-              </div>
-
-              <div className="price-column">
-                <p className="product-price">${product.productPrice}</p>
-              </div>
-
-              <div className="prod-total-column">
-                <p className="product-total-price">
-                  $
-                  {product.productTotal
-                    .toString()
-                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-                </p>
-              </div>
+              ))}
             </div>
-          ))}
-        </div>
-        {error && <div className="error-message">{error}</div>}{" "}
-        <div className="cart-total">
-          <h3>Order Total:</h3>
-          <p className="price-fineprint">
-            (Total is pre-tax; Shipping is FREE!)
-          </p>
-          <p className="cart-total-dollars">
-            $
-            {cartProducts
-              .reduce(
-                (total, product) => total + parseFloat(product.productTotal),
-                0
-              )
-              .toFixed(2)
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
-          </p>
-        </div>
-        <div className="cart-buttons">
-          <button className="cart-button" onClick={handleShopping}>
-            <h4 className="checkout-text">Keep Shopping!</h4>
-          </button>
-          <button className="cart-button" onClick={handleCheckout}>
-            <h4 className="checkout-text">Check out!</h4>
-          </button>
-        </div>
+            {error && <div className="error-message">{error}</div>}
+            <div className="cart-total">
+              <h3>Order Total:</h3>
+              <p className="price-fineprint">
+                (Total is pre-tax; Shipping is FREE!)
+              </p>
+              <p className="cart-total-dollars">
+                $
+                {cartProducts
+                  .reduce(
+                    (total, product) =>
+                      total + parseFloat(product.productTotal),
+                    0
+                  )
+                  .toFixed(2)
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </p>
+            </div>
+            <div className="cart-buttons">
+              <button className="cart-button" onClick={handleShopping}>
+                <h4 className="checkout-text">Keep Shopping!</h4>
+              </button>
+              <button className="cart-button" onClick={handleCheckout}>
+                <h4 className="checkout-text">Check out!</h4>
+              </button>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
